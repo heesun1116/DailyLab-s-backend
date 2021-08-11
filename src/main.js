23152c117ba757479912a098dda8df29bb78a36e
heesun@gamil.com <70732062+heesun1116@users.noreply.github.com>
2021-08-12 07:54:33 +0900

    build

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 //서버와 데이터베이스의 연결
 import mongoose from 'mongoose';
-
+import path from 'path';
+import serve from 'koa-static';
+import send from 'koa-send';
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
 
@@ -36,7 +38,16 @@ app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 
 //Use 4000 if no PORT is specified
-
+const buildDirectory = path.resolve(
+  __dirname,
+  '../../DailyLabs-frontend/build',
+);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 const port = PORT || 4000;
 app.listen(port, () => {
   console.log('Listenging to port %d', port);
